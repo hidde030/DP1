980053bcfa4f9d7d7aa9b537e05032b15d2bdd4d
Hidde Glansdorp <hidde.glansdorp@student.hu.nl>
2022-09-12 11:08:04 +0200

    Update p1

--- src/main/java/org/example/p1/Main.java
+++ src/main/java/org/example/p1/Main.java
@@ -6,33 +6,25 @@ import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:postgresql://localhost/test";
-        //DB NAME = test
+        String url = "jdbc:postgresql://localhost/ov-chipkaart";
+        //DB NAME = ov-chipkaart
         //DB USERNAME = postgres
         //DB PASSWORD = root
-
         Properties props = new Properties();
         props.setProperty("user","postgres");
         props.setProperty("password","root");
-        props.setProperty("ssl","true");
+//        props.setProperty("ssl","false");
         Connection conn = DriverManager.getConnection(url, props);
 
-//        String url = "jdbc:postgresql://localhost/test?user=postgres&password=root&ssl=false";
-//        Connection conn = DriverManager.getConnection(url);
-//        Statement st = conn.createStatement();
-//        ResultSet rs = st.executeQuery("SELECT * FROM studenten");
-//        while (rs.next())
-//        {
-//            System.out.println(rs.getString(2));
-//        }
-//        rs.close();
-//        st.close();
-
-
-        LocalDate localDate = LocalDate.now();
-        PreparedStatement st = conn.prepareStatement("INSERT INTO studenten (id,naam) VALUES (4,?)");
-        st.setObject(1, localDate);
-        st.executeUpdate();
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM reiziger");
+        System.out.println("Alle reizigers: ");
+        while (rs.next())
+        {
+            System.out.println("#" + rs.getString(1) + ". " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4));
+        }
+        rs.close();
         st.close();
+
     }
 }
