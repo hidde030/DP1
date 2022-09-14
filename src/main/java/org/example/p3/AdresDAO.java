2f499e9761554b269a91dce1738bc49f143e75c7
hidde030 <glansdorphidde@gmail.com>
2022-09-14 20:58:27 +0200

    p3

---
+++ src/main/java/org/example/p3/AdresDAO.java
@@ -0,0 +1,6 @@
+package org.example.p3;
+
+public interface AdresDAO {
+
+
+}
---
+++ src/main/java/org/example/p3/Main.java
@@ -0,0 +1,8 @@
+package org.example.p3;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
